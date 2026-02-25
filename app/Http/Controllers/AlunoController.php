a52c7daf98c73ff0713e1cb98044633f400ff083
unknown <joaoeddgamer@gmail.com>
2026-02-25 17:14:06 -0300

    comitando umas parada ae

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -3,14 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Aluno;
 
 class AlunoController extends Controller
 {
 
     function index(){
 
-        
+
         return view('aluno.list');
+    }
+
+    function create(){
+        Aluno::create([
+            'nome' => 'João',
+            'email' => '123.456.789-00',
+            'telefone' => '123456789'
+        ]);
+
+        return view('aluno.create');
     }
 
 }
